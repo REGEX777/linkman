c188ff7d1c5365ee7e5c96a429d48c2f9e917bd2
REGEX777 <hmmho705@gmail.com>
2024-08-23 21:59:05 +0530

    Added charts for countries

--- routes/analyse.js
+++ routes/analyse.js
@@ -5,15 +5,11 @@ const router = express.Router();
 
 router.get('/:redirectString', async (req, res) => {
     try {
-        const {
-            redirectString
-        } = req.params;
+        const { redirectString } = req.params;
         console.log('received redirectString:', redirectString);
-
-        const link = await Link.findOne({
-            redirectString
-        }).exec();
-
+        
+        const link = await Link.findOne({ redirectString }).exec();
+        
         if (!link) {
             console.log('Link not found');
             return res.status(404).send('Link not found');
@@ -21,6 +17,7 @@ router.get('/:redirectString', async (req, res) => {
 
         const visits = link.visits;
 
+        // Aggregate daily visits
         const dailyVisits = visits.reduce((acc, visit) => {
             const visitDate = new Date(visit.date).toDateString();
             if (!acc[visitDate]) {
@@ -30,7 +27,7 @@ router.get('/:redirectString', async (req, res) => {
             return acc;
         }, {});
 
-        // PLEASE FIND THE COUNTRIES FROM THE DATABASE ILL KILL YOU
+        // Aggregate visits by country
         const countryVisits = visits.reduce((acc, visit) => {
             if (acc[visit.country]) {
                 acc[visit.country] += visit.totalVisits;
@@ -40,16 +37,20 @@ router.get('/:redirectString', async (req, res) => {
             return acc;
         }, {});
 
+        // Convert to an array of objects for easier sorting and chart data
         const sortedCountries = Object.entries(countryVisits)
-            .map(([country, visits]) => ({
-                country,
-                visits
-            }))
-            .sort((a, b) => b.visits - a.visits);
-
-        res.render('analyse', {
-            linkUrl: `https://domain.com/${redirectString}`,
+            .map(([country, visits]) => ({ country, visits }))
+            .sort((a, b) => b.visits - a.visits); // Sort by visits, descending
+        
+        // Separate country names and visit counts for chart
+        const countryNames = sortedCountries.map(item => item.country);
+        const countryVisitCounts = sortedCountries.map(item => item.visits);
+        
+        res.render('analyse', { 
+            linkUrl: `https://domain.com/${redirectString}`, 
             dailyVisits: JSON.stringify(dailyVisits),
+            countryNames: JSON.stringify(countryNames),
+            countryVisitCounts: JSON.stringify(countryVisitCounts),
             sortedCountries: sortedCountries
         });
     } catch (error) {
