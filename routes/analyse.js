95801b98b7e990a4a8e401c8f19fe2b9dd845089
REGEX777 <hmmho705@gmail.com>
2024-08-23 20:54:06 +0530

    Added the top countries list

--- routes/analyse.js
+++ routes/analyse.js
@@ -1,15 +1,19 @@
 import express from 'express';
-import Link from '../models/Link.js'; 
+import Link from '../models/Link.js';
 
 const router = express.Router();
 
 router.get('/:redirectString', async (req, res) => {
     try {
-        const { redirectString } = req.params;
+        const {
+            redirectString
+        } = req.params;
         console.log('received redirectString:', redirectString);
-        
-        const link = await Link.findOne({ redirectString }).exec();
-        
+
+        const link = await Link.findOne({
+            redirectString
+        }).exec();
+
         if (!link) {
             console.log('Link not found');
             return res.status(404).send('Link not found');
@@ -19,19 +23,34 @@ router.get('/:redirectString', async (req, res) => {
 
         const dailyVisits = visits.reduce((acc, visit) => {
             const visitDate = new Date(visit.date).toDateString();
-
             if (!acc[visitDate]) {
                 acc[visitDate] = 0;
             }
             acc[visitDate] += visit.count;
-
             return acc;
         }, {});
-        
-        
-        res.render('analyse', { 
-            linkUrl: `https://domain.com/${redirectString}`, 
-            dailyVisits: JSON.stringify(dailyVisits)
+
+        // PLEASE FIND THE COUNTRIES FROM THE DATABASE ILL KILL YOU
+        const countryVisits = visits.reduce((acc, visit) => {
+            if (acc[visit.country]) {
+                acc[visit.country] += visit.totalVisits;
+            } else {
+                acc[visit.country] = visit.totalVisits;
+            }
+            return acc;
+        }, {});
+
+        const sortedCountries = Object.entries(countryVisits)
+            .map(([country, visits]) => ({
+                country,
+                visits
+            }))
+            .sort((a, b) => b.visits - a.visits);
+
+        res.render('analyse', {
+            linkUrl: `https://domain.com/${redirectString}`,
+            dailyVisits: JSON.stringify(dailyVisits),
+            sortedCountries: sortedCountries
         });
     } catch (error) {
         console.error('Server Error:', error);
