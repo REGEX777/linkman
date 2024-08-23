b80ef0d6fc03e44672919f75c10c57f8d99b7783
REGEX777 <hmmho705@gmail.com>
2024-08-23 19:44:36 +0530

    fixed the graph

--- routes/analyse.js
+++ routes/analyse.js
@@ -5,26 +5,36 @@ const router = express.Router();
 
 router.get('/:redirectString', async (req, res) => {
     try {
-        const redString = req.params.redirectString
-        const link = await Link.findOne({ redirectString: redString }); 
+        const { redirectString } = req.params;
+        console.log('received redirectString:', redirectString);
+        
+        const link = await Link.findOne({ redirectString }).exec();
+        
+        if (!link) {
+            console.log('Link not found');
+            return res.status(404).send('Link not found');
+        }
+
         const visits = link.visits;
 
         const dailyVisits = visits.reduce((acc, visit) => {
             const visitDate = new Date(visit.date).toDateString();
+
             if (!acc[visitDate]) {
                 acc[visitDate] = 0;
             }
             acc[visitDate] += visit.count;
+
             return acc;
         }, {});
-        console.log(dailyVisits);
+        
         
         res.render('analyse', { 
-            linkUrl: `https://domain.com/${link.redirectString}`, 
+            linkUrl: `https://domain.com/${redirectString}`, 
             dailyVisits: JSON.stringify(dailyVisits)
         });
     } catch (error) {
-        console.error(error);
+        console.error('Server Error:', error);
         res.status(500).send('Server Error');
     }
 });
