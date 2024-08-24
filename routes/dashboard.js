7bd2fb35a270ea821bbefad653d3781d61f7e818
REGEX777 <hmmho705@gmail.com>
2024-08-24 20:53:48 +0530

    sorting is sorted

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,5 @@
 import express from 'express';
 
-
-import { requireLogin } from '../middleware/requireLogin.js';
 // model import
 
 import Link from '../models/Link.js'
@@ -9,9 +7,30 @@ import Link from '../models/Link.js'
 const router = express.Router();
 
 
-router.get('/', requireLogin, async (req, res)=>{
-    try{    
-        const links = await Link.find({})
+router.get('/', async (req, res)=>{
+    try{
+        let sortCriteria = {};
+        // switch case for sorting
+        switch (req.query.sort) {
+            case 'new':
+                sortCriteria = { createdAt: -1 }; 
+                break;
+            case 'old':
+                sortCriteria = { createdAt: 1 }; 
+                break;
+            case 'most-visits':
+                sortCriteria = { visits: -1 }; 
+                break;
+            case 'least-visits':
+                sortCriteria = { visits: 1 }; 
+                break;
+            default:
+                sortCriteria = { createdAt: -1 }; 
+                break;
+        }
+
+        const links = await Link.find({ owner: req.user._id }).sort(sortCriteria);
+
         res.render('dashboard', {links: links})
     }catch(err){
         console.log(err);
