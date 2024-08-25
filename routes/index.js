c11c0e67b6b3ceeec45b17748b5f358c694fe351
REGEX777 <hmmho705@gmail.com>
2024-08-25 16:21:41 +0530

    Revert "refactored the index backend route"
    This reverts commit 1561b144885dcd77fd4f73e4ef0755f0274e0d98.

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,10 @@ import qrcode from 'qrcode';
 
 import { requireLogin } from '../middleware/requireLogin.js';
 import prankMiddleware from '../middleware/prank.js';
-import isValidUrl from '../middleware/urlCheck.js';
-import Link from '../models/Link.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const configPath = path.join(__dirname, '../config.json');
-
 function getConfig() {
     try {
         const configData = fs.readFileSync(configPath, 'utf8');
@@ -37,24 +34,33 @@ function getCountry(ip) {
     });
 }
 
+
 const config = getConfig();
+
 const debug = true;
 
-function random5digitString(length) {
+function random5digitString(length){
     return crypto.randomBytes(length)
-        .toString('base64')
-        .replace(/[^a-zA-Z0-9]/g, '')
-        .slice(0, length);
+    .toString('base64')
+    .replace(/[^a-zA-Z0-9]/g, '')
+    .slice(0, length)
 }
+
+// Middlware import
+import isValidUrl from '../middleware/urlCheck.js';
+
+// model import
+import Link from '../models/Link.js'
 
 const router = express.Router();
 
-router.get('/', requireLogin, (req, res) => {
+router.get('/', requireLogin, (req, res)=>{
     res.render('index', { 
         error: req.flash('error'), 
         success: req.flash('success') 
-    });
-});
+    })
+})
+
 
 router.get('/api/:redirectString', prankMiddleware, async (req, res) => {
     try {
@@ -64,68 +70,68 @@ router.get('/api/:redirectString', prankMiddleware, async (req, res) => {
         if (!link) {
             return res.status(404).send("Not Found");
         }
+        
+        if (link && link.active) { 
+            link.visits.push({ count: 1, date: new Date() });
+            await link.save();
+            res.redirect(link.url);
+        } else {
+            res.status(404).send('Link not found or inactive.');
+        }
+
+        const ip = "8.8.8.8"; 
+        let country;
 
         const now = new Date();
         if (link.expirationDate && now > link.expirationDate) {
             return res.status(410).send("This link has expired.");
         }
 
-        if (link.active) { 
-            link.visits.push({ count: 1, date: new Date() });
-            await link.save();
-
-            const ip = "8.8.8.8"; 
-            let country;
-
-            try {
-                country = await getCountry(ip); 
-                if (!country) {
-                    console.error('country info undefined');
-                    return res.status(500).send('failed to get country info');
-                }
-            } catch (err) {
-                console.error('error while trying to get country:', err);
+        try {
+            country = await getCountry(ip); 
+            if (!country) {
+                console.error('country info undegined');
                 return res.status(500).send('failed to get country info');
-            }
-
-            link.totalVisits += 1;
-
-            let countryVisit = link.visits.find(v => v.country === country);
-
-            if (countryVisit) {
-                countryVisit.count += 1;
-                console.log('updated countryVisit:', countryVisit);
-            } else {
-                link.visits.push({ country, count: 1 });
-                console.log('new countryVisit:', { country, count: 1 });
-            }
-
-            const today = new Date();
-            today.setHours(0, 0, 0, 0);
-
-            let todayVisit = link.visits.find(visit => {
-                if (!visit.date) {
-                    return false;
-                }
-                const visitDate = new Date(visit.date);
-                visitDate.setHours(0, 0, 0, 0);
-                return visitDate.getTime() === today.getTime();
-            });
-
-            if (todayVisit) {
-                todayVisit.count += 1;
-                console.log('updated todayVisit:', todayVisit);
-            } else {
-                link.visits.push({ date: today, count: 1 });
-                console.log('new todayVisit:', { date: today, count: 1 });
-            }
-
-            await link.save();
-            res.redirect(link.url);
-
-        } else {
-            res.status(404).send('Link not found or inactive.');
+            } 
+        } catch (err) {
+            console.error('error while trying to get country:', err);
+            return res.status(500).send('failed to get country info');
         }
+        link.totalVisits += 1;
+
+        let countryVisit = link.visits.find(v => v.country === country);
+
+        if (countryVisit) {
+            countryVisit.count += 1;
+            console.log('updated countryVisit:', countryVisit);
+        } else {
+            link.visits.push({ country, count: 1 });
+            console.log('new countryVisit:', { country, count: 1 });
+        }
+
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        let todayVisit = link.visits.find(visit => {
+            if (!visit.date) {
+                return false;
+            }
+            const visitDate = new Date(visit.date);
+            visitDate.setHours(0, 0, 0, 0);
+            return visitDate.getTime() === today.getTime();
+        });
+
+        if (todayVisit) {
+            todayVisit.count += 1;
+            console.log('updated todayVisit:', todayVisit);
+        } else {
+            link.visits.push({ date: today, count: 1 });
+            console.log('new todayVisit:', { date: today, count: 1 });
+        }
+
+        await link.save();
+
+        res.redirect(link.url);
 
     } catch (err) {
         console.error('error in /api/:redirectString route:', err);
@@ -135,14 +141,19 @@ router.get('/api/:redirectString', prankMiddleware, async (req, res) => {
 
 router.post('/search', async (req, res) => {
     try {
-        const searchQuery = req.body.searchQuery;
-        const links = await Link.find({ name: { $regex: searchQuery, $options: 'i' } });
-        res.render('search', { links });
+      const searchQuery = req.body.searchQuery;
+
+      const links = await Link.find({
+        name: { $regex: searchQuery, $options: 'i' }
+      });
+  
+      res.render('search', { links });
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Server Error');
+      console.error(error);
+      res.status(500).send('Server Error');
     }
 });
+  
 
 router.post('/', requireLogin, isValidUrl, async (req, res) => {
     try {
@@ -154,7 +165,7 @@ router.post('/', requireLogin, isValidUrl, async (req, res) => {
         if (customKeyword) {
             const existingLink = await Link.findOne({ redirectString: customKeyword });
             if (existingLink) {
-                return res.render('error', { error: 'Custom keyword is already taken, please enter something else.' });
+                return res.render('error', { error: 'Custom keyword is already takes please enter something else.' });
             }
         } else {
             customKeyword = random5digitString(5);
@@ -169,7 +180,7 @@ router.post('/', requireLogin, isValidUrl, async (req, res) => {
 
         const link = new Link({
             url: url,
-            redirectString: customKeyword,
+            redirectString: randString,
             expirationDate: expirationDate,
             name: req.body.name
         });
@@ -185,11 +196,12 @@ router.post('/', requireLogin, isValidUrl, async (req, res) => {
 
         const shortUrl = `${config.domain}/${link.redirectString}`;
 
-        qrcode.toDataURL(shortUrl, (err, qrCodeUrl) => {
+        qrcode.toDataURL(shortUrl, function (err, qrCodeUrl) {
             if (err) {
                 console.log(err);
                 return res.render('error', { error: 'failed to generate QR code.' });
             }
+
             res.render('success', { shortUrl, qrCodeUrl });
         });
 
@@ -200,27 +212,30 @@ router.post('/', requireLogin, isValidUrl, async (req, res) => {
 
 router.post('/pin/:id', requireLogin, async (req, res) => {
     const { id } = req.params;
-    const { pinned } = req.body;
-
+    const { pinned } = req.body; 
+    
     try {
         await Link.findByIdAndUpdate(id, { pinned: pinned });
-        res.redirect('/dashboard');
+        res.redirect('/dashboard'); 
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
     }
 });
 
+
 router.delete('/delete/:id', requireLogin, async (req, res) => {
     try {
         const linkId = req.params.id;
+        
         const link = await Link.findOne({ _id: linkId, owner: req.user._id });
-
+        
         if (!link) {
             return res.status(404).send('Link not found or you do not have permission to delete it.');
         }
 
         await Link.deleteOne({ _id: linkId });
+
         res.redirect('/dashboard');
     } catch (err) {
         console.error(err);
@@ -239,5 +254,6 @@ router.post('/toggle-active/:id', requireLogin, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
 
 export default router;
